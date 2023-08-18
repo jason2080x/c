2d7a1484016c8a03821ed99665b59fe15cc0d7c6
jason2080x <jason2080x@outlook.com>
2023-08-19 03:18:19 +0800

    07

--- json-to-yaml/src/main.js
+++ json-to-yaml/src/main.js
@@ -1,19 +1,64 @@
 const yaml = require("js-yaml")
 document.addEventListener("DOMContentLoaded", () => {
-  const txtYaml = document.getElementById("txt-yaml")
-  const txtJson = document.getElementById("txt-json")
-  const customEvent = new Event("input", { bubbles: true, cancelable: false })
-  txtJson.addEventListener("input", () => {
-    if (txtJson.value.trim()) {
+  const txtInput = document.getElementById("txt-input")
+  const txtOutput = document.getElementById("txt-output")
+  const inputImportFromFile = document.getElementById("input-import-from-file")
+  const inputCopyToClipboard = document.getElementById("input-copy-to-clipboard")
+  const outputImportFromFile = document.getElementById("output-import-from-file")
+  const outputCopyToClipboard = document.getElementById("output-copy-to-clipboard")
+  const inputFile = document.getElementById("input-file")
+  const outputFile = document.getElementById("output-file")
+  txtInput.addEventListener("input", () => {
+    if (txtInput.value.trim()) {
       try {
-        const obj = JSON.parse(txtJson.value)
-        txtYaml.value = yaml.dump(obj)
+        const obj = JSON.parse(txtInput.value)
+        txtOutput.value = yaml.dump(obj)
       } catch (e) {
-        txtYaml.value = e.message
+        txtOutput.value = e.message
       }
     } else {
-      txtYaml.value = ""
+      txtOutput.value = ""
     }
-    txtYaml.dispatchEvent(customEvent)
+    txtOutput.dispatchEvent(new Event("input", { bubbles: true, cancelable: false }))
+  })
+  // 从文件导入
+  inputImportFromFile.addEventListener("click", () => {
+    inputFile.click()
+  })
+  inputFile.addEventListener("change", (event) => {
+    const file = event.target.files[0]
+    let reader = new FileReader()
+    reader.onload = (() => {
+      return function (e) {
+        txtInput.value = e.target.result
+        txtInput.dispatchEvent(new Event("input", { bubbles: true, cancelable: false }))
+      }
+    })(file)
+    reader.readAsText(file)
+    inputFile.value = ""
+  })
+  outputImportFromFile.addEventListener("click", () => {
+    outputFile.click()
+  })
+  outputFile.addEventListener("change", (event) => {
+    const file = event.target.files[0]
+    let reader = new FileReader()
+    reader.onload = (() => {
+      return function (e) {
+        txtOutput.value = e.target.result
+        txtOutput.dispatchEvent(new Event("input", { bubbles: true, cancelable: false }))
+      }
+    })(file)
+    reader.readAsText(file)
+    outputFile.value = ""
+  })
+  // 复制
+  inputCopyToClipboard.addEventListener("click", () => {
+    navigator.clipboard.writeText(txtInput.value)
+    new bootstrap.Modal(document.getElementById("modal"), {}).show()
+  })
+  outputCopyToClipboard.addEventListener("click", () => {
+    navigator.clipboard.writeText(txtOutput.value)
+    new bootstrap.Modal(document.getElementById("modal"), {}).show()
   })
 })
--- yaml-to-json/src/main.js
+++ yaml-to-json/src/main.js
@@ -1,19 +1,64 @@
 const yaml = require("js-yaml")
 document.addEventListener("DOMContentLoaded", () => {
-  const txtYaml = document.getElementById("txt-yaml")
-  const txtJson = document.getElementById("txt-json")
-  const customEvent = new Event("input", { bubbles: true, cancelable: false })
-  txtYaml.addEventListener("input", () => {
-    if (txtYaml.value.trim()) {
+  const txtInput = document.getElementById("txt-input")
+  const txtOutput = document.getElementById("txt-output")
+  const inputImportFromFile = document.getElementById("input-import-from-file")
+  const inputCopyToClipboard = document.getElementById("input-copy-to-clipboard")
+  const outputImportFromFile = document.getElementById("output-import-from-file")
+  const outputCopyToClipboard = document.getElementById("output-copy-to-clipboard")
+  const inputFile = document.getElementById("input-file")
+  const outputFile = document.getElementById("output-file")
+  txtInput.addEventListener("input", () => {
+    if (txtInput.value.trim()) {
       try {
-        const obj = yaml.load(txtYaml.value)
-        txtJson.value = JSON.stringify(obj, null, 2)
+        const obj = yaml.load(txtInput.value)
+        txtOutput.value = JSON.stringify(obj, null, 2)
       } catch (e) {
-        txtJson.value = e.message
+        txtOutput.value = e.message
       }
     } else {
-      txtJson.value = ""
+      txtOutput.value = ""
     }
-    txtJson.dispatchEvent(customEvent)
+    txtOutput.dispatchEvent(new Event("input", { bubbles: true, cancelable: false }))
+  })
+  // 从文件导入
+  inputImportFromFile.addEventListener("click", () => {
+    inputFile.click()
+  })
+  inputFile.addEventListener("change", (event) => {
+    const file = event.target.files[0]
+    let reader = new FileReader()
+    reader.onload = (() => {
+      return function (e) {
+        txtInput.value = e.target.result
+        txtInput.dispatchEvent(new Event("input", { bubbles: true, cancelable: false }))
+      }
+    })(file)
+    reader.readAsText(file)
+    inputFile.value = ""
+  })
+  outputImportFromFile.addEventListener("click", () => {
+    outputFile.click()
+  })
+  outputFile.addEventListener("change", (event) => {
+    const file = event.target.files[0]
+    let reader = new FileReader()
+    reader.onload = (() => {
+      return function (e) {
+        txtOutput.value = e.target.result
+        txtOutput.dispatchEvent(new Event("input", { bubbles: true, cancelable: false }))
+      }
+    })(file)
+    reader.readAsText(file)
+    outputFile.value = ""
+  })
+  // 复制
+  inputCopyToClipboard.addEventListener("click", () => {
+    navigator.clipboard.writeText(txtInput.value)
+    new bootstrap.Modal(document.getElementById("modal"), {}).show()
+  })
+  outputCopyToClipboard.addEventListener("click", () => {
+    navigator.clipboard.writeText(txtOutput.value)
+    new bootstrap.Modal(document.getElementById("modal"), {}).show()
   })
 })
